62d5a30e3423bdd37b8b698f22efaff60ed1a607
Lollipop69 <94169753+lollipop-69@users.noreply.github.com>
2022-01-22 13:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import nextcord
+from nextcord import Embed, Color
 from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix = "?",case_insensitive = True, strip_after_prefix = True)
+
+@bot.event
+async def on ready():
+    print("BOT IS READY")
+  
+@bot.command()
+async def ping(ctx):
+    embed = Embed(title = "Latency", description = f"{round(bot.latency*1000)}",color = Color.random())
+    await ctx.send(embed=embed)
+    
+bot.run("TOKEN")
