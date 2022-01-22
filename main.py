a9546b811b19ad47e4b1a47b74944592d33da2cb
Lollipop69 <94169753+lollipop-69@users.noreply.github.com>
2022-01-22 16:37:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,74 @@ bot = commands.Bot(command_prefix = "?",case_insensitive = True, strip_after_pre
 @bot.event # event to be triggered when the bot is ready
 async def on ready():
     print("BOT IS READY")
+    
+    
+    
+ 
+
+@bot.event
+
+async def on_guild_join(guild):
+
+    with open('./databases/prefixes.json','r') as f:
+
+        prefixes=json.load(f)
+
+    prefixes[str(guild.id)]='?'
+
+    with open('./databases/prefixes.json','w') as f:
+
+        json.dump(prefixes,f,indent=4)
+
+@bot.event
+
+async def on_guild_remove(guild):
+
+    with open('./databases/prefixes.json','r') as f:
+
+        prefixes=json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('./databases/prefixes.json','w') as f:
+
+        json.dump(prefixes,f,indent=4)
+
+        
+
+@bot.command(aliases = ['setprefix'])
+
+@commands.guild_only()
+
+@commands.has_permissions(administrator=True)
+
+async def prefix(ctx,prefix):
+
+    a = ctx.guild.get_member(bot.user.id)
+
+    j = "Ghost Boy"
+
+    nick = f"[{prefix}] {j}"
+
+    await a.edit(nick=nick)
+
+    with open('./databases/prefixes.json','r') as f:
+
+        prefixes=json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open('./databases/prefixes.json','w') as f:
+
+        json.dump(prefixes,f,indent=4)
+
+    jhg=discord.Embed(description=f'**Prefix of the bot for the server has been changed to {prefix}**',color=discord.Color.from_rgb(000,000,205))
+
+    await ctx.send(embed=jhg)   
+    
+    
+    
+    
   
 @bot.command() # a simple ping command to know the websocket latency
 async def ping(ctx):
