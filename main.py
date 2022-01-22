f87a16b3f750b01bd74c626902b69b61cb1b8dbc
Lollipop69 <94169753+lollipop-69@users.noreply.github.com>
2022-01-22 13:37:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import nextcord
-from nextcord import Embed, Color
-from nextcord.ext import commands
+import nextcord # importing nextcord Library to run the bot
+from nextcord import Embed, Color # importing Embed and Color
+from nextcord.ext import commands # importing the main component 
 
-bot = commands.Bot(command_prefix = "?",case_insensitive = True, strip_after_prefix = True)
+bot = commands.Bot(command_prefix = "?",case_insensitive = True, strip_after_prefix = True) # creating a bot variable to set a prefix for the bot
 
-@bot.event
+@bot.event # event to be triggered when the bot is ready
 async def on ready():
     print("BOT IS READY")
   
-@bot.command()
+@bot.command() # a simple ping command to know the websocket latency
 async def ping(ctx):
     embed = Embed(title = "Latency", description = f"{round(bot.latency*1000)}",color = Color.random())
     await ctx.send(embed=embed)
