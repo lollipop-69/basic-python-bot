39654a9c3e3a77f5c2b385860ee2152c51f5528d
Lollipop69 <94169753+lollipop-69@users.noreply.github.com>
2022-01-26 13:58:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,19 @@ import nextcord # importing nextcord Library to run the bot
 from nextcord import Embed, Color # importing Embed and Color
 from nextcord.ext import commands # importing the main component 
 
-
 def get_prefix(bot,message):
-
     if message.guild:
-
-    
-
         with open('./databases/prefixes.json','r') as f:
-
             prefixes=json.load(f)
-
-    
-
         return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot,message)
-
     if not message.guild:
-
         return "?"
-
-
 
 
 bot = commands.Bot(command_prefix = get_prefix ,case_insensitive = True, strip_after_prefix = True) # creating a bot variable to set a prefix for the bot
 
 @bot.event # event to be triggered when the bot is ready
-async def on ready():
+async def on_ready():
     print("BOT IS READY")
     
     
@@ -35,67 +22,37 @@ async def on ready():
  
 
 @bot.event
-
 async def on_guild_join(guild):
-
     with open('./databases/prefixes.json','r') as f:
-
         prefixes=json.load(f)
-
     prefixes[str(guild.id)]='?'
-
     with open('./databases/prefixes.json','w') as f:
-
         json.dump(prefixes,f,indent=4)
 
 @bot.event
-
 async def on_guild_remove(guild):
-
     with open('./databases/prefixes.json','r') as f:
-
         prefixes=json.load(f)
-
     prefixes.pop(str(guild.id))
-
     with open('./databases/prefixes.json','w') as f:
-
         json.dump(prefixes,f,indent=4)
-
-        
+      
 
 @bot.command(aliases = ['setprefix'])
-
 @commands.guild_only()
-
 @commands.has_permissions(administrator=True)
-
 async def prefix(ctx,prefix):
-
     a = ctx.guild.get_member(bot.user.id)
-
-    j = "Ghost Boy"
-
+    j = "Your Bot Name"
     nick = f"[{prefix}] {j}"
-
     await a.edit(nick=nick)
-
     with open('./databases/prefixes.json','r') as f:
-
         prefixes=json.load(f)
-
     prefixes[str(ctx.guild.id)] = prefix
-
     with open('./databases/prefixes.json','w') as f:
-
         json.dump(prefixes,f,indent=4)
-
     jhg=discord.Embed(description=f'**Prefix of the bot for the server has been changed to {prefix}**',color=Color.from_rgb(000,000,205))
-
-    await ctx.send(embed=jhg)   
-    
-    
-    
+    await ctx.send(embed=jhg)       
     
   
 @bot.command() # a simple ping command to know the websocket latency
